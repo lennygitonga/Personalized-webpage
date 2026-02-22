883f8b9929267b59f0c9d3c59d31e8725c3c7664
Lenny Gitonga <lennygitonga07@gmail.com>
2026-02-23 00:53:56 +0300

    add getitem localstorage

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 let form = document.querySelector('form');
 
 form.addEventListener('submit', function(e) {
+    e.preventDefault();
     let name = document.querySelector('#name').value
     let age = document.querySelector('#age').value
 
     localStorage.setItem('name', name);
     localStorage.setItem('age', age);
+
+    localStorage.getItem('name');
+    localStorage.getItem('age');
 });
